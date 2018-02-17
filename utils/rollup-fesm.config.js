05cc0a31364a068e225be0ac183e654001d93162
ngx-devtools <aelbore.sg@gmail.com>
2018-02-17 12:53:57 +0800

    fix the rollup config change

--- utils/rollup-fesm.config.js
+++ utils/rollup-fesm.config.js
@@ -1,11 +1,13 @@
 const config = require('./bundle-config');
 const utils = require('./bundle-utils');
 
-module.exports = (folder) => {
+module.exports = folder => {
   return {
     input: `${config.folder.tmp}/${folder}/${config.rollup.entry}`,
-    external: config.rollup.external,
-    format: 'es',
+    output: {
+      format: 'es',
+      external: config.rollup.external
+    },
     allowRealFiles: true,
     plugins: utils.plugins,
     onwarn: utils.onwarn
--- utils/rollup-umd.config.js
+++ utils/rollup-umd.config.js
@@ -3,11 +3,13 @@ const utils = require('./bundle-utils');
 
 module.exports = {
   input: `${config.folder.tmp}/esm5/${config.rollup.entry}`,
-  format: 'umd',
-  exports: 'named',
-  name: config.rollup.moduleName,
-  globals: config.rollup.globals,
-  external: config.rollup.external,
+  output: {
+    format: 'umd',
+    name: config.rollup.moduleName,
+    exports: 'named',
+    globals: config.rollup.globals,
+    external: config.rollup.external
+  },
   allowRealFiles: true,
   plugins: utils.plugins,
   onwarn: utils.onwarn
